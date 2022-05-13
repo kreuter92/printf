0c5175f1cdffa48c153fdaa33555b554db608a69
kreuter92 <kristina.reuter@outlook.de>
2022-05-13 21:23:38 +0200

    Painted the face colorful

---
+++ ansi_color.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	printf("Hello World\n");
+	printf("\e[0;33m ######\e[0m\n");
+	printf("#\e[0;36m O  O \e[0m#\n");
+	printf("\e[0;37m#  {}  #\e[0m\n");
+	printf("#\e[0;35m ____\e[0m #\n");
+	printf("\e[0;37m#      #\e[0m\n");
+	printf("\e[0;37m ###### \e[0m\n");
+	
+	return 0;
+}
